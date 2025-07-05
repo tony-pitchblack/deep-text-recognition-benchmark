f76764218ac59241bbf5d7e762f75071baec2596
Anton Chernov <tony.pitchblack@yandex.ru>
2025-07-05 19:24:50 +0300

    add script for downloading pretrained models - for now support only best model

---
+++ download_pretrained.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+gdown -O ckpt/pretrained/TPS-ResNet-BiLSTM-Attn.pth --fuzzy \
+    https://drive.google.com/file/d/1ajONZOgiG9pEYsQ-eBmgkVbMDuHgPCaY/view?usp=drive_link
